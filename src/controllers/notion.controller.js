797e72a51e11b5f41c7d9bffc11a51d506de5c3f
8three <cmg8three@gmail.com>
2023-11-25 22:42:02 +0100

    Refactor router

--- src/controllers/notion.controller.js
+++ src/controllers/notion.controller.js
@@ -2,20 +2,26 @@ const { getPriceAllUseCase, getPriceByRangeDateUseCase } = require("../use_cases
 const { getAllUseCase, getByRangeDateUseCase } = require("../use_cases/records.usecases");
 
 
-async function getRecords() {
-  return getAllUseCase();
+async function getRecords(req, res) {
+  const notionRecords = await getAllUseCase();
+  res.status(200).json(notionRecords);
 }
 
-async function getRecordsFilteredByDateRage(startDate, endDate) {
-  return getByRangeDateUseCase(startDate, endDate);
+async function getRecordsFilteredByDateRage(req, res) {
+  const { startDate, endDate } = req.body;
+  const notionRecords = await getByRangeDateUseCase(startDate, endDate);
+  res.status(200).json(notionRecords);
 }
 
-async function getRecordsFilteredByDateRageCalculate(startDate, endDate) {
-  return getPriceByRangeDateUseCase(startDate, endDate);
+async function getRecordsFilteredByDateRageCalculate(req, res) {
+  const { startDate, endDate } = req.body;
+  const notionRecords = await getPriceByRangeDateUseCase(startDate, endDate);
+  res.status(200).json(notionRecords);
 }
 
-async function getRecordsCalculate() {
-  return getPriceAllUseCase();
+async function getRecordsCalculate(req, res) {
+  const notionRecords = await getPriceAllUseCase();
+  res.status(200).json(notionRecords);
 }
 
 module.exports = {
--- src/routes/notion.router.js
+++ src/routes/notion.router.js
@@ -1,44 +1,24 @@
 const express = require('express');
 const _ = require('underscore');
 
-const notionRouter = express.Router();
+const router = express.Router();
 
 const notionController = require('../controllers/notion.controller');
 
-notionRouter.get('/records', (req, res) => {
-    return notionController
-        .getRecords()
-        .then(notionRecords => {
-            console.log('Recupera los registros de Notion!!');
-            res.status(200).json(notionRecords);
-        })
-})
-
-notionRouter.post('/records', (req, res) => {
-    const { startDate, endDate } = req.body;
-    return notionController
-        .getRecordsFilteredByDateRage(startDate, endDate)
-        .then(notionRecords => {
-            res.status(200).json(notionRecords);
-        })
-})
-
-notionRouter.get('/records/calculate', (req, res) => {
-    return notionController
-        .getRecordsCalculate()
-        .then(price => {
-            res.status(200).json(price);
-        })
-})
-
-notionRouter.post('/records/calculate', (req, res) => {
-    const { startDate, endDate } = req.body;
-    return notionController
-        .getRecordsFilteredByDateRageCalculate(startDate, endDate)
-        .then(notionRecords => {
-            res.status(200).json(notionRecords);
-        })
-})
+router.route('/records')
+    .get(notionController.getRecords);
 
 
-module.exports = notionRouter
+router.route('/records')
+    .post(notionController.getRecordsFilteredByDateRage);
+
+
+router.route('/records/calculate')
+    .get(notionController.getRecordsCalculate);
+
+
+router.route('/records/calculate')
+    .post(notionController.getRecordsFilteredByDateRageCalculate);
+
+
+module.exports = router
