06c9d6a36484bc69e3b6723404340572a928f42e
8three <cmg8three@gmail.com>
2023-11-29 13:34:23 +0100

    fix mapper

--- src/utils/mapper-response.js
+++ src/utils/mapper-response.js
@@ -1,7 +1,8 @@
 function mapperRespone(data) {
   let result = [];
-  for (let propertie of data.results) {
-    result.push(getPropertie(propertie.properties));
+  for (let field of data.results) {
+    if (!checkProperties(field.properties)) continue;
+    result.push(getPropertie(field.properties));
   }
   return result;
 }
@@ -13,6 +14,29 @@ function getPropertie(prop) {
     price: prop['Price'].number,
     tag: prop['Tag'].select.name,
   }
+}
+
+function checkProperties(properties) {
+  return propDdate(properties) &&
+    propInfo(properties) &&
+    propPrice(properties) &&
+    propTag(properties);
+}
+
+function propDdate(properties) {
+  return !!properties['Date'].date;
+}
+
+function propInfo(properties) {
+  return !!properties['info'].rich_text[0].plain_text;
+}
+
+function propPrice(properties) {
+  return !!properties['Price'].number;
+}
+
+function propTag(properties) {
+  return !!properties['Tag'].select.name;
 }
 
 module.exports = {
