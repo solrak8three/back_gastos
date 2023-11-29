699fed5ab8c0d06c13268e06a8c4c58e984048f5
8three <cmg8three@gmail.com>
2023-11-29 12:09:40 +0100

    fix

--- src/use_cases/records.usecases.js
+++ src/use_cases/records.usecases.js
@@ -5,13 +5,13 @@ const { filterByDates } = require("../utils/dates");
 async function recordsFilteredUseCase(startDate, endDate, tags) {
   let records = await getRecords();
   records = [...filterByTags(records, tags)];
-  return [...filterByDates(startDate, endDate, tags)]
+  return filterByDates(records, startDate, endDate);
 }
 
 async function recordsFilteredOnlyFixedUseCase(startDate, endDate, tags) {
   let records = await getRecords();
   records = [...filterByTagsOnlyFixed(records, tags)];
-  return [...filterByDates(startDate, endDate, tags)]
+  return filterByDates(records, startDate, endDate);
 }
 
 module.exports = {
